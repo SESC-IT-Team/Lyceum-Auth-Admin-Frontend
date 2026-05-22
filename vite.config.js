baf7dfc47d89ff70d266abe6601d6ee89dd72e81
Bykova_Sv <145900679+sveta0202@users.noreply.github.com>
2026-05-22 20:23:54 +0500

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,11 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     host: '0.0.0.0',
+    allowedHosts: ['sesc-it-team.ru', 'auth.sesc-it-team.ru'],
   },
   preview: {
     host: '0.0.0.0',
     port: 4173,
+    allowedHosts: ['sesc-it-team.ru', 'auth.sesc-it-team.ru'],
   },
 })
