9770ba5fda631c62ee7645c15fa36078e1d8d121
MushroomForYou <k.i.vetoshkin@gmail.com>
2026-08-05 22:26:44 +0200

    style: format App component return statement

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import UserAdminPage from '@/components/auth-admin/UsersAdminPage';
 
 export default function App() {
-    return (
-        <UserAdminPage />
-    )
+  return (
+      <UserAdminPage />
+  )
 }
